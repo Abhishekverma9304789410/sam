14291e90bab17fd3efabab7e6e52eb503518a776
Abhishekverma9304789410 <158255472+Abhishekverma9304789410@users.noreply.github.com>
2024-02-06 14:07:55 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,13 @@
+public class sum {
+        public static void main (String[] args){
+                int a = 4;
+                int b =3;
+                int c = 6;
+                int sum = a+ b +c;
+                System.out.println(sum);
+                
+
+
+        }
+        
+}
